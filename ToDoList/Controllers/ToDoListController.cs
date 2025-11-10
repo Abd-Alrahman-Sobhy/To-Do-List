464eab57ef44c6b1a7e858005c7cbaf1c52334ee
Abd Elrahman Sobhy <abdalrahman.sobhy14@gmail.com>
2025-11-10 12:53:11 +0200

    Initial

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public ToDoListController(UserManager<User> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult CreateToDoTask()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateToDoTask(ToDoTask toDoTask)
+        {
+            if (!ModelState.IsValid || toDoTask == null)
+            {
+                return View();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            var item = new ToDoTask
+            {
+                Title = toDoTask.Title,
+                DeadLine = toDoTask.DeadLine,
+                UserId = Convert.ToString(user.Id)
+            };
+
+            await _context.Tasks.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("ToDoListTabel");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ToDoListTabel()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var toDoItems = await _context.Tasks.Where(t => t.UserId == user.Id).ToListAsync();
+
+            return View(toDoItems);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var item = await _context.Tasks.FindAsync(id);
+            if (item != null)
+            {
+                _context.Tasks.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("ToDoListTabel");
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoTask.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDoTask
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime? DeadLine { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public User? user { get; set; }
+    }
+}
